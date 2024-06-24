62d8374ce8fd67e119007102718d52e7b8882c2a
Yuzu <Yuzu_0204@outlook.com>
2024-06-24 18:27:48 -0400

    feat: Implement Swiper carousel in work page
    This commit adds the Swiper dependency to the project and updates the work page (page.tsx) to implement a carousel using Swiper. The carousel allows for the display of multiple project images in a visually appealing way, enhancing the user experience.

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -17,5 +17,45 @@ import "swiper/css";
 import { ProjectsData } from "./Data/Projects";
 
 export default function Work() {
-  return <div> work page</div>;
+  const [projectsData, setProjectsData] = useState(ProjectsData[0]);
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="mih-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%} xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div>
+              {/* Outline Number */}
+              <div className="text-8xl leading-normal font-extrabold text-transparent text-outline">
+                {projectsData.num}
+              </div>
+              {/* Project Category */}
+              <h2
+                className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500
+              capitalize"
+              >
+                {projectsData.category}
+              </h2>
+              {/* Project Description */}
+              <p className="text-white/60">{projectsData.description}</p>
+              {/* Stack */}
+              <ul>
+                {projectsData.stack.map((item, index) => (
+                  <li key={item.name} className="text-accent text-xl">
+                    {item.name}
+                    {/* Remove the last comma */}
+                    {index !== projectsData.stack.length - 1 && ", "}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%}">slider</div>
+        </div>
+      </div>
+    </motion.section>
+  );
 }
