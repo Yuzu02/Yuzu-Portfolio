e63447a02310433c18a5e1ef7a1b29a5aaf2ea2e
Yuzu <Yuzu_0204@outlook.com>
2024-06-24 16:51:23 -0400

    feat: Add Swiper dependency and update work page
    This commit adds the Swiper dependency to the project by updating the package.json file. It also imports the necessary components and modules in the work page (page.tsx) to implement a carousel using Swiper. This enhancement improves the user experience by allowing the display of multiple project images in a visually appealing way.

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/common/tooltip";
+import Link from "next/link";
+import Image from "next/image";
+import "swiper/css";
+
+// Data
+import { ProjectsData } from "./Data/Projects";
+
 export default function Work() {
   return <div> work page</div>;
 }
