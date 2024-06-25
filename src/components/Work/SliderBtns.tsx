dcce4bb402de9875b6a2b1fce3a1a4a7a9bab22e
Yuzu <Yuzu_0204@outlook.com>
2024-06-25 03:37:42 -0400

    feat: Add Swiper carousel to work page

--- src/components/Work/SliderBtns.tsx
+++ src/components/Work/SliderBtns.tsx
@@ -1,3 +1,33 @@
-export default function SliderBtns() {
-  return <div>WorkSliderBtns</div>;
+"use client";
+
+import { useSwiper } from "swiper/react";
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
+
+export default function SliderBtns({
+  containerStyles,
+  btnStyles,
+  iconStyles,
+}: Readonly<{
+  containerStyles: string;
+  btnStyles: string;
+  iconStyles: string;
+}>) {
+  const swiper = useSwiper();
+
+  return (
+    <div className={containerStyles}>
+      <button className={btnStyles}>
+        <PiCaretLeftBold
+          className={iconStyles}
+          onClick={() => swiper.slidePrev()}
+        />
+      </button>
+      <button className={btnStyles}>
+        <PiCaretRightBold
+          className={iconStyles}
+          onClick={() => swiper.slideNext()}
+        />
+      </button>
+    </div>
+  );
 }
