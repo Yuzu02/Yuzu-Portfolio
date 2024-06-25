2cef40e0d01bb3ecf9879f99e0bc0f986f17505d
Yuzu <Yuzu_0204@outlook.com>
2024-06-25 01:46:59 -0400

    feat: Add Swiper carousel to work page
    This commit adds the Swiper carousel to the work page, allowing for the display of multiple project images in a visually appealing way. It enhances the user experience and improves the presentation of the portfolio website.

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -16,18 +16,32 @@ import "swiper/css";
 // Data
 import { ProjectsData } from "./Data/Projects";
 
+// Components
+import SliderBtns from "@/components/Work/SliderBtns";
+
 export default function Work() {
   const [projectsData, setProjectsData] = useState(ProjectsData[0]);
+
+  const handleSlideChange = (swiper: { activeIndex: string | number }) => {
+    // get the current project index
+    const currentProject = swiper.activeIndex;
+    // set the current project data
+    setProjectsData(ProjectsData[currentProject as number]);
+  };
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
       className="mih-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:w-[50%} xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-            <div>
+            <div className="flex flex-col gap-[30px] h-[50%]">
               {/* Outline Number */}
               <div className="text-8xl leading-normal font-extrabold text-transparent text-outline">
                 {projectsData.num}
@@ -42,7 +56,7 @@ export default function Work() {
               {/* Project Description */}
               <p className="text-white/60">{projectsData.description}</p>
               {/* Stack */}
-              <ul>
+              <ul className="flex gap-4">
                 {projectsData.stack.map((item, index) => (
                   <li key={item.name} className="text-accent text-xl">
                     {item.name}
@@ -51,9 +65,69 @@ export default function Work() {
                   </li>
                 ))}
               </ul>
+              {/* Border */}
+              <div className="border border-white/20"></div>
+              {/* Button */}
+              <div className="flex items-center gap-4">
+                {/* Live Project Button */}
+                <Link href={projectsData.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Live Project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+                {/* Github Project Button */}
+                <Link href={projectsData.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Github Repository</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
             </div>
           </div>
-          <div className="w-full xl:w-[50%}">slider</div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {ProjectsData.map((project) => {
+                return (
+                  <SwiperSlide key={project.num} className="w-full xl:w-[50%]">
+                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                      {/* Overlay */}
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                      {/* Image */}
+                      <div className="w-full h-full relative">
+                        <Image
+                          src={project.image}
+                          alt={project.title}
+                          fill
+                          className="object-cover object-center w-full h-full"
+                        />
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+              {/* Slider Button */}
+              <SliderBtns />
+            </Swiper>
+          </div>
         </div>
       </div>
     </motion.section>
---
+++ src/components/Work/SliderBtns.tsx
@@ -0,0 +1,3 @@
+export default function SliderBtns() {
+  return <div>WorkSliderBtns</div>;
+}
