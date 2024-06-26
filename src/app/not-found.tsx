a86ffacac0a609e2bcbb31dcab115b412d9cc1fc
Yuzu <Yuzu_0204@outlook.com>
2024-06-26 03:44:02 -0400

    refactor : Add Redirect to Root Page and Layout

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center h-screen text-2xl font-bold text-white">
+      404 - Page Not Found
+    </div>
+  );
+}
