0c7e7eb3bcfa067a12dd45eaf05e3dec8fa02b54
Yuzu <Yuzu_0204@outlook.com>
2024-07-07 07:35:09 -0400

    Deploy -- Error Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function RootPage() {
-  const userLanguage = navigator.language;
-
-  if (userLanguage.startsWith("es")) {
-    redirect("/es");
-  } else {
-    redirect("/en");
-  }
+  redirect("/en");
 }
